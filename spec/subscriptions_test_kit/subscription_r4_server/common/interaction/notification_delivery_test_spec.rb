2b52ea1141796aae0ee121c4956806dba1374f7a
Stephen MacVicar <smacvicar@mitre.org>
2024-11-18 10:25:04 -0500

    fix linting error

--- spec/subscriptions_test_kit/subscription_r4_server/common/interaction/notification_delivery_test_spec.rb
+++ spec/subscriptions_test_kit/subscription_r4_server/common/interaction/notification_delivery_test_spec.rb
@@ -2,7 +2,6 @@ require_relative '../../../../../lib/subscriptions_test_kit/suites/subscriptions
                  'common/interaction/notification_delivery_test'
 
 RSpec.describe SubscriptionsTestKit::SubscriptionsR5BackportR4Server::NotificationDeliveryTest, :request do
-
   let(:suite) { Inferno::Repositories::TestSuites.new.find('subscriptions_r5_backport_r4_server') }
   let(:test) { Inferno::Repositories::Tests.new.find('subscriptions_r4_server_notification_delivery') }
   let(:test_group) { Inferno::Repositories::TestGroups.new.find('subscriptions_r4_server_interaction') }

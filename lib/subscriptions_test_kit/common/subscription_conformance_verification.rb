f8e025ab84a42c96b8b0f6a9d768ab120f008ad4
Karl Naden <86612928+karlnaden@users.noreply.github.com>
2024-12-30 10:35:30 -0500

    condition fix
    Co-authored-by: Tom Strassner <tstrassner@mitre.org>

--- lib/subscriptions_test_kit/common/subscription_conformance_verification.rb
+++ lib/subscriptions_test_kit/common/subscription_conformance_verification.rb
@@ -77,7 +77,7 @@ module SubscriptionsTestKit
           Added the Authorization header field with a Bearer token set to #{access_token} to the `header` field on the
           Subscription resource in order to connect successfully with the Inferno subscription channel.
         ))
-        subscription_channel['header'] = [] unless subscription['header'].present?
+        subscription_channel['header'] = [] unless subscription_channel['header'].present?
         subscription_channel['header'].append("Authorization: Bearer #{access_token}")
       end
       subscription['channel'] = subscription_channel

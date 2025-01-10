b679c30b128d41dba870a0a1d4642fc11a1a7020
Karl Naden <knaden@mitre.org>
2025-01-10 14:23:07 -0500

    re-apply typo fixes across rename

--- lib/subscriptions_test_kit/suites/subscriptions_r5_backport_r4_server/subscription_rejection/reject_subscription_channel_payload_combo_test.rb
+++ lib/subscriptions_test_kit/suites/subscriptions_r5_backport_r4_server/subscription_rejection/reject_subscription_channel_payload_combo_test.rb
@@ -9,7 +9,7 @@ module SubscriptionsTestKit
       title 'Server Handles Unsupported Subscription Payload for Channel Type'
       description %(
         When processing a request for a Subscription a server SHOULD verify that the Subscription is supported and does
-        not contain any information not implemented by the server. If the Subscription is no supported, the server
+        not contain any information not implemented by the server. If the Subscription is not supported, the server
         should reject the Subscription create request, or it should attempt to adjust the Subscription. When
         processing a request for a Subscription, a server SHOULD validate, that the payload configuration is
         valid for the channel type requested (e.g., complies with the server's security policy).

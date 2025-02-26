9c808c6f862272229171ec6a01799727eaa6b4b3
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-02-26 10:37:07 -0500

    Release 0.10.0 (#20)
    * Bump version to 0.10.0
    
    * Update CHANGELOG
    
    ---------
    
    Co-authored-by: Tom Strassner <tstrassner@mitre.org>

--- lib/subscriptions_test_kit/version.rb
+++ lib/subscriptions_test_kit/version.rb
@@ -1,4 +1,4 @@
 module SubscriptionsTestKit
-  VERSION = '0.9.4'.freeze
-  LAST_UPDATED = '2025-02-04'.freeze # TODO: update next release
+  VERSION = '0.10.0'.freeze
+  LAST_UPDATED = '2025-02-25'.freeze
 end

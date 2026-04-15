51cac4cddb4559ffb25566e2ea008daf174e55d6
FlexonyoPizza <LeonDhoska@gmail.com>
2026-04-15 12:17:42 -0400

    YAML files for server and client execution scripts

---
+++ execution_scripts/advance_wait.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Advances an Inferno wait state by sending a GET request to the given URL.
+# An optional delay can be specified to allow background jobs to complete
+# before advancing (e.g. waiting for notifications to be delivered).
+#
+# Args:
+#   ARGV[0] - url          (the wait URL to GET)
+#   ARGV[1] - delay        (seconds to sleep before advancing, default: 0)
+
+require 'net/http'
+require 'uri'
+
+url   = ARGV[0]
+delay = ARGV[1]&.to_i || 0
+
+raise "Usage: #{$0} <url> [delay_seconds]" if url.nil?
+
+if delay > 0
+  puts "Waiting #{delay}s for notifications to be delivered..."
+  sleep delay
+end
+
+puts "Advancing wait: #{url}"
+uri      = URI(url)
+response = Net::HTTP.new(uri.host, uri.port).request(Net::HTTP::Get.new(uri.request_uri))
+puts "Response: #{response.code}"
